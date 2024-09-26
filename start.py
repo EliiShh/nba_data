ca48c7b768c29bcfe10bc2023026a851bbe6489c
EliiShh <eliyahushmul@gmail.com>
2024-09-26 14:00:00 +0300

    עדכון עם תאריך ספציפי

--- start.py
+++ start.py
@@ -1,10 +1,4 @@
-# from service.player_service import *
-#
-# players_f_api = read_players_from_json("../assets/data.json")
-#
-# players = list_players_from_api_map_to_model(players_f_api)
-#
-# create_players_in_db(players)
+
 
 
 #psql -U postgres
